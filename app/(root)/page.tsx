c9752cb25c03a116ab566560999f919aac61bc62
= <=>
2023-08-28 01:27:17 -0500

    +modals +providers +global-modal-store +form-basic

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,23 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      This is a protected Route!!
-      <UserButton afterSignOutUrl="/" />
+      Root Page
     </main>
   );
 };
