b5576d4ecc441b338c45cf55ef2f63e5d8b4f50d
= <=>
2023-08-28 19:35:37 -0500

    +multiple entities (color, sizes, billboards) +p:color-picker, react-spinners

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
-
 import { ModalProvider } from "@/providers/modal-provider";
-import prismadb from "@/lib/prismadb";
 import { ToastProvider } from "@/providers/toast-provider";
 
 const inter = Inter({ subsets: ["latin"] });
