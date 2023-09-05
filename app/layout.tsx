abe1a580174aafedefbe2c799c6dbe51399c0c4e
= <=>
2023-09-05 00:17:04 -0500

    +fix multiple children injection in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,6 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            {children}
-
             <ToastProvider />
             <ModalProvider />
             {children}
